0f3a613177fd4ad0e340e581f8408b8255d027ce
Anyars Yussif <anyarsencarta@gmail.com>
2024-01-06 16:02:11 +0000

    Update generateJoke.js

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -6,7 +6,7 @@ function generateJoke () {
             Accept: 'application/json',
         },
     }
-// fetch jokes from https://icanhazdadjoke.com API
+// fetch jokes from https://icanhazdadjoke.com 
     axios.get('https://icanhazdadjoke.com', config).then((res) => {
         document.getElementById('joke').innerHTML = res.data.joke
     })
